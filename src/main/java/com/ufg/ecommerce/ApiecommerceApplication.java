e53c54d3b8ebd4e86324c1717299f31eae0efa0b
Balmore Cruz <analista2@saram.com>
2023-04-19 11:11:15 -0600

    primer commit

---
+++ src/main/java/com/ufg/ecommerce/ApiecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ufg.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiecommerceApplication.class, args);
+	}
+
+}
